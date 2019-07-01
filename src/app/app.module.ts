d3829012d072beda5f65fe8a6cf70e125012d1f4
Tova <Tova@rachip.com>
2019-07-01 15:54:21 +0300

    Start create comp & service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,20 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ClarityModule } from '@clr/angular';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NewRoomComponent } from './new-room/new-room.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewRoomComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ClarityModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
